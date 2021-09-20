4791b8440babebb843e05cdd5acc563aba2a714b
Larry Chiem <clarry@pdx.edu>
2021-09-20 01:54:48 -0700

    created chatengine.io

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import { ChatEngine } from 'react-chat-engine';
+
+import './App.css';
+
+const App = () => {
+    return (
+        <ChatEngine
+            height="100vh"
+            projectID="caa41485-fa6a-4529-a0b2-4a7d4f019607"
+            userName="larrychiem"
+            userSecret="123123"
+        />
+    );
+}
+
+export default App;
